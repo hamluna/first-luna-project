c03747acf79d39d934b0592fac431be593b29732
luna <hamchyang@gmail.com>
2025-07-25 11:00:32 +0900

    Replace old project with new one

---
+++ script/menu_ani.js
@@ -0,0 +1,9 @@
+
+function activateMenu() {
+  $(".nav > ul > li").mouseover(function(){
+      $(this).find(".submenu").stop().slideDown();
+  });
+  $(".nav > ul > li").mouseout(function(){
+      $(this).find(".submenu").stop().slideUp();
+  });
+}
